a75202c6d2f2664b640c388259bdabad18155c1d
Erkesh <erkeshs@mail.ru>
2024-08-04 18:14:23 +0500

    update

--- Lesson-11/user-data.sh
+++ Lesson-11/user-data.sh
@@ -5,8 +5,8 @@ sudo yum -y install httpd
 
 sudo cat <<EOF > /var/www/html/index.html
 <html>
-<h1> Made by Terraform <font color="red"> v.1.5.6</font></h1></br>
-<h2>Owner <font color="green">${f_name} ${l_name}.</font></h2></br>
+<h1> Made by Terraform <font color="red"> v.1.9.3</font></h1></br>
+<h2> Owner <font color="green">${f_name} ${l_name}.</font></h2></br>
 EOF
 
 sudo systemctl enable httpd
